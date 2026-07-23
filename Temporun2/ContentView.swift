d6f7f5ca605b4b6c8d78b28d83f7107cf1d55373
sshouchiku <ssshouchiku@gmail,com>
2026-07-24 08:43:16 +0900

    Initial Commit

---
+++ Temporun2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Temporun2
+//
+//  Created by matsuyama on 2026/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Temporun2/Temporun2App.swift
@@ -0,0 +1,17 @@
+//
+//  Temporun2App.swift
+//  Temporun2
+//
+//  Created by matsuyama on 2026/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Temporun2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
